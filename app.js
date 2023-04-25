5a15edfb26791931649f5bfd2f8e35f6c0a186cc
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2023-04-26 00:31:37 +0300

    added 2fa setup

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { Pool } = require('pg');
 const argon2 = require('argon2');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const speakeasy = require('speakeasy');
 
+/*
+  Initial configuration
+ */
 dotenv.config();
 
 const passwordPepper = process.env.PASSWORD_PEPPER;
+const jwtSecret = process.env.JWT_SECRET;
 
 
 const dbPool = new Pool(); // uses process.env.PG* variables by default
@@ -16,6 +22,32 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+/*
+  Middlewares
+ */
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, jwtSecret, (err, user) => {
+            if (err) {
+                return res.status(403).send('Invalid token');
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+/*
+  Endpoints
+ */
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -56,7 +88,6 @@ app.post('/users', async (req, res) => {
         return;
     }
 
-    // todo: sanitize input to avoid injections
     dbPool.query('insert into users (username, password_hash, email) values ($1, $2, $3)', [username, hash, email], (err, result) => {
         if (err) {
             console.error(err);
@@ -79,8 +110,7 @@ app.get('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    //todo: sanitize input to avoid injections
-    dbPool.query('select password_hash from users where username = $1', [username], async (err, result) => {
+    dbPool.query('select password_hash, user_id, twofa_secret from users where username = $1', [username], async (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).send('Internal server error');
@@ -92,9 +122,12 @@ app.get('/login', (req, res) => {
 
             const hash = result.rows[0].password_hash;
             const valid = await argon2.verify(hash, password + passwordPepper);
+
+            const user_id = result.rows[0].user_id;
             if (valid) {
-                // todo JWT
-                res.status(200).send('Login successful');
+                const twoFactorRequired = result.rows[0].twofa_secret !== null;
+
+                res.status(200).send(jwt.sign({ username, user_id, twofa_passed: !twoFactorRequired }, jwtSecret));
             } else {
                 res.status(401).send('Invalid username or password');
             }
@@ -102,4 +135,95 @@ app.get('/login', (req, res) => {
     });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+/**
+ * @api {post} /login/2fa add 2nd factor
+ */
+app.post('/login/2fa', authenticateJWT, (req, res) => {
+    if (req.user.twofa_passed) {
+        res.status(400).send('2nd factor already enabled');
+        return;
+    }
+
+    // confirm that there is no 2nd factor already enabled in the db
+    dbPool.query('select twofa_secret from users where user_id = $1', [req.user.user_id], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal server error');
+        } else {
+            if (result.rows[0].twofa_secret !== null) {
+                res.status(400).send('2nd factor already enabled');
+                return;
+            }
+        }
+    });
+
+    // generate a new secret
+    const secret = speakeasy.generateSecret({
+        issuer: process.env.APP_NAME, 
+        name: req.user.username,
+        length: 20 
+    });
+
+    // save the secret in the db
+    dbPool.query('update users set twofa_secret = $1 where user_id = $2', [secret.base32, req.user.user_id], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal server error');
+        } else {
+            res.status(200).send(secret.otpauth_url);
+        }
+    });
+
+});
+
+/**
+ * @api {get} /login/2fa/verify confirm enabling 2fa by verifying a code
+ */
+app.get('/login/2fa/verify', authenticateJWT, (req, res) => {
+    if (req.user.twofa_passed) {
+        res.status(400).send('already logged in');
+        return;
+    }
+
+    if (!req.body || !req.body.otp) {
+        res.status(400).send('missing required fields');
+        return;
+    }
+
+    const otp = req.body.otp;
+
+    dbPool.query('select twofa_secret from users where user_id = $1', [req.user.user_id], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal server error');
+        } else {
+            if (result.rows[0].twofa_secret === null) {
+                res.status(400).send('2nd factor not enabled');
+                return;
+            }
+
+            const verified = speakeasy.totp.verify({
+                secret: result.rows[0].twofa_secret,
+                encoding: 'base32',
+                token: otp
+            });
+
+            if (verified) {
+                // confirm verified 2fa in db
+                dbPool.query('update users set twofa_confirmed = true where user_id = $1', [req.user.user_id], (err, result) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send('Internal server error');
+                    }
+                });
+
+                res.status(200).send(jwt.sign({ username: req.user.username, user_id: req.user.user_id, twofa_passed: true }, jwtSecret));
+            } else {
+                res.status(401).send('Invalid code');
+            }
+        }
+    });
+
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
