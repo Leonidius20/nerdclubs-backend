d777ca92235fcf2722387f1f042f648d96ac4a93
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2023-05-23 22:00:00 +0300

    started implementing moderators and categories

--- routes/communities.routes.js
+++ routes/communities.routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import controller from '../controllers/communities.controller.js';
-import authenticateJWT from '../middlewares/authenticate.middleware.js';
+import authenticateJWT, { authenticateJWTOptional } from '../middlewares/authenticate.middleware.js';
+import moderatorsRouter from './moderators.routes.js';
+import extractCommunityUrl from '../middlewares/extract.community.url.js';
 
 const router = Router();
 
@@ -9,7 +11,15 @@ const router = Router();
  * If no seach query is specified, it will return all communities
  */
 router.get('/', controller.getAll);
-router.get('/:url', controller.getByUrl);
+
+router.get('/:url', authenticateJWTOptional, controller.getByUrl);
 router.post('/', authenticateJWT, controller.create);
+
+// add sub-route for moderators of a community
+
+
+// router.use('/:url/moderators', extractCommunityUrl, moderatorsRouter);
+
+// maybe add sub-route for categories of a community
 
 export default router;
