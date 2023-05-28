f20ce648a9cd628ca05da21a41a2a38f8f3d3922
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2023-05-28 23:43:06 +0300

    refactored post votes controller

--- controllers/post.votes.controller.js
+++ controllers/post.votes.controller.js
@@ -1,4 +1,5 @@
 import { dbPool } from "../services/db.service.js";
+import firstRowOrThrow from "../utils/firstRowOrThrow.js";
 
 export default {
     voteForPost,
@@ -6,27 +7,20 @@ export default {
     getMyVoteForPost,
 };
 
-async function voteForPost(req, res) {
+async function voteForPost(req, res, next) {
     const user_id = req.user.user_id;
 
     const { post_id, is_positive } = req.body;
 
-    if (post_id == null || is_positive == null) {
-        res.status(400).json({error: 1, message: 'Missing post_id or is_positive'});
-        return;
-    }
-
     try {
-        const result = await dbPool.query('insert into post_votes (post_id, user_id, is_positive) values ($1, $2, $3) on conflict (post_id, user_id) do update set is_positive = $3', [post_id, user_id, is_positive]);
+        await dbPool.query('insert into post_votes (post_id, user_id, is_positive) values ($1, $2, $3) on conflict (post_id, user_id) do update set is_positive = $3', [post_id, user_id, is_positive]);
         res.json({success: true});
     } catch (err) {
-        console.error(err);
-        res.status(500).json({error: 2, message: 'Internal server error'});
-        return;
+        next(err);
     }
 }
 
-async function getVotesForPost(req, res) {
+async function getVotesForPost(req, res, next) {
     const post_id = req.params.post_id;
 
     if (!post_id) {
@@ -36,19 +30,13 @@ async function getVotesForPost(req, res) {
 
     try {
         const result = await dbPool.query('select COALESCE(sum(CASE WHEN is_positive THEN 1 ELSE -1 END),0) as rating from post_votes where post_id = $1', [post_id]);
-        if (result.rows.length == 0) {
-            res.status(404).json({error: 3, message: 'Post not found'});
-            return;
-        }
-        res.json(result.rows[0]); // {rating: 0}
+        res.json(firstRowOrThrow(result)); // {rating: 0}
     } catch (err) {
-        console.error(err);
-        res.status(500).json({error: 2, message: 'Internal server error'});
-        return;
+        next(err);
     }
 }
 
-async function getMyVoteForPost(req, res) {
+async function getMyVoteForPost(req, res, next) {
     const user_id = req.user.user_id;
     const post_id = req.params.post_id;
 
@@ -65,8 +53,6 @@ async function getMyVoteForPost(req, res) {
         }
         res.json({ i_voted: true, is_positive:  result.rows[0].is_positive}); // {is_positive: true}
     } catch (err) {
-        console.error(err);
-        res.status(500).json({error: 2, message: 'Internal server error'});
-        return;
+        next(err);
     }
 }
--- routes/post.votes.routes.js
+++ routes/post.votes.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import controller from "../controllers/post.votes.controller.js";
 import authenticateJWT from "../middlewares/authenticate.middleware.js";
+import checkBodyFieldsCurry from "../middlewares/check.body.fields.middleware.js";
 
 const router = Router();
 
-router.post("/", authenticateJWT, controller.voteForPost);
+router.post("/", 
+    authenticateJWT, 
+    checkBodyFieldsCurry(["post_id", "is_positive"]), 
+    controller.voteForPost
+);
 router.get("/:post_id", controller.getVotesForPost);
 router.get("/:post_id/my", authenticateJWT, controller.getMyVoteForPost);
 
