d37703c0b704b0d6fea54aeddb8a6602149dfaa8
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2023-06-18 16:09:33 +0300

    banned users in com pagination

--- controllers/community.bans.controller.js
+++ controllers/community.bans.controller.js
@@ -9,10 +9,19 @@ export default {
 async function getAllBannedUsersInCommunity(req, res, next) {
     const { community_id } = req.body;
 
+    let { page } = req.query;
+
+    if (!page) {
+        page = 1;
+    }
+
+    const pageSize = 10; 
+    const offset = (page - 1) * pageSize;
+
     try {
         const { rows } = await dbPool.query(
-            'SELECT u.user_id, u.username FROM community_bans cb LEFT JOIN users u ON cb.user_id = u.user_id WHERE cb.community_id = $1',
-            [community_id]
+            'SELECT u.user_id, u.username, count(*) OVER() AS full_results_count FROM community_bans cb LEFT JOIN users u ON cb.user_id = u.user_id WHERE cb.community_id = $1 ORDER BY username LIMIT $2 OFFSET $3',
+            [community_id, pageSize, offset]
         );
             
         res.status(200).json(rows);
